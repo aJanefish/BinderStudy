a89f135e62dab3408611950c15ac9ccf6da039c3
zhangyu <5686648+zhangyuwxf@user.noreply.gitee.com>
2023-03-01 09:06:36 +0800

    opt:move app

---
+++ business/frame/server-impl/src/main/java/com/zy/server/install/ApkInstallManager.java
@@ -0,0 +1,162 @@
+package com.zy.server.install;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Parcel;
+import android.os.RemoteCallbackList;
+import android.os.RemoteException;
+
+import com.example.ipc.ApkInfo;
+import com.example.ipc.IApkInstallListener;
+import com.example.ipc.IApkInstallManager;
+import com.zy.zlog.ZLog;
+
+import java.util.Arrays;
+
+public class ApkInstallManager extends IApkInstallManager.Stub {
+
+
+    private static final String TAG = "ApkInstallManager";
+    private int flag;
+
+    private final String PER_PERMISSION = "com.example.binderservice.TEST_ACCESS";
+    private final RemoteCallbackList<IApkInstallListener> listeners = new RemoteCallbackList<>();
+
+    private final Context context;
+
+
+    ApkInstallManager(Context context) {
+        this.context = context;
+    }
+
+
+    @Override
+    public boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+        //return super.onTransact(code, data, reply, flags);
+        int checkCallingOrSelfPermission = context.checkCallingOrSelfPermission(PER_PERMISSION);
+        ZLog.d(TAG, "onTransact check:" + checkCallingOrSelfPermission + " " + Thread.currentThread());
+        if (checkCallingOrSelfPermission == PackageManager.PERMISSION_DENIED) {
+            //权限不满足，返回NULL，绑定服务失败
+            return false;
+        }
+
+        String pkgName = null;
+        int callingUid = getCallingUid();
+        int callingPid = getCallingPid();
+        ZLog.d(TAG, "onTransact callingUid:" + callingUid + " callingPid:" + callingPid);
+        String[] packages = context.getPackageManager().getPackagesForUid(callingUid);
+        ZLog.d(TAG, "onTransact packages:" + Arrays.toString(packages));
+        if (packages != null && packages.length > 0) {
+            pkgName = packages[0];
+        }
+
+        //限定某些包名可以访问服务
+        if (!"com.example.ipcdemo".equals(pkgName)) {
+            return false;
+        }
+
+        return super.onTransact(code, data, reply, flags);
+    }
+
+    @Override
+    public void setFlag(int flag) throws RemoteException {
+        this.flag = flag;
+        ZLog.d(TAG, "setFlag:" + flag + " " + Thread.currentThread() + " " + ZLog.getStackTraceString(new Throwable()));
+    }
+
+    @Override
+    public int getFlag() throws RemoteException {
+        ZLog.d(TAG, "getFlag:" + flag + " " + Thread.currentThread() + " " + ZLog.getStackTraceString(new Throwable()));
+        return this.flag;
+    }
+
+    @Override
+    public boolean checkPermission() throws RemoteException {
+        ZLog.d(TAG, "checkPermission:" + Thread.currentThread());
+        return false;
+    }
+
+    @Override
+    public void startSilentInstall(ApkInfo info) throws RemoteException {
+        ZLog.d(TAG, "startSilentInstall:" + info + " " + Thread.currentThread() + " " + ZLog.getStackTraceString(new Throwable()));
+        startSilentInstallImp(info);
+    }
+
+    @Override
+    public void startCommonInstall(ApkInfo info) throws RemoteException {
+        ZLog.d(TAG, "startInstall:" + info + " " + Thread.currentThread() + " " + ZLog.getStackTraceString(new Throwable()));
+        startInstallImp(info);
+    }
+
+    private void startInstallImp(ApkInfo info) throws RemoteException {
+        int n = listeners.beginBroadcast();
+        for (int i = 0; i < n; i++) {
+            IApkInstallListener iApkInstallListener = listeners.getBroadcastItem(i);
+            if (iApkInstallListener != null) {
+                iApkInstallListener.onStatusChanged(1, "start common Install");
+                iApkInstallListener.onStatusChanged(2, "check apk");
+                info.setFilePath("" + info.getFilePath() + "...checked");
+                iApkInstallListener.onStatusChanged(3, "Installing...");
+                iApkInstallListener.onStatusChanged(4, "Install success");
+                iApkInstallListener.onStatusChanged(5, "Install failed");
+            }
+        }
+        listeners.finishBroadcast();
+    }
+
+    private void startSilentInstallImp(final ApkInfo info) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int n = listeners.beginBroadcast();
+                for (int i = 0; i < n; i++) {
+                    IApkInstallListener iApkInstallListener = listeners.getBroadcastItem(i);
+                    if (iApkInstallListener != null) {
+                        try {
+                            ZLog.d(TAG, "1 start silent Install");
+                            iApkInstallListener.onStatusChanged(1, "start silent Install");
+                            ZLog.d(TAG, "2 check apk");
+                            iApkInstallListener.onStatusChanged(2, "check apk");
+                            ZLog.d(TAG, "3 Installing...");
+                            info.setFilePath("" + info.getFilePath() + "...checked");
+                            iApkInstallListener.onStatusChanged(3, "Installing...");
+                            ZLog.d(TAG, "4 Install success");
+                            iApkInstallListener.onStatusChanged(4, "Install success");
+                            ZLog.d(TAG, "5 Install failed");
+                            iApkInstallListener.onStatusChanged(5, "Install failed");
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                            ZLog.d(TAG, "startSilentInstallImp:" + e);
+                        }
+                    }
+                }
+                listeners.finishBroadcast();
+            }
+        }).start();
+    }
+
+    @Override
+    public void registerListener(IApkInstallListener listener) throws RemoteException {
+        try {
+            listeners.register(listener);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        int sum = listeners.beginBroadcast();
+        listeners.finishBroadcast();
+        ZLog.d(TAG, "registerListener:" + sum + " " + listener + " " + Thread.currentThread() + " " + ZLog.getStackTraceString(new Throwable()));
+        ZLog.d(TAG, "registerListener asBinder:" + listener.asBinder());
+    }
+
+    @Override
+    public void unregisterListener(IApkInstallListener listener) throws RemoteException {
+        try {
+            listeners.unregister(listener);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        int sum = listeners.beginBroadcast();
+        listeners.finishBroadcast();
+        ZLog.d(TAG, "unregisterListener:" + sum + " " + listener + " " + Thread.currentThread());
+    }
+}
---
+++ business/frame/server-impl/src/main/java/com/zy/server/install/ApkTestService.java
@@ -0,0 +1,71 @@
+package com.zy.server.install;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.zy.zlog.ZLog;
+
+public class ApkTestService extends Service {
+
+    private static final String TAG = "ApkTestService";
+
+    public ApkTestService() {
+    }
+
+    private Binder mBinder = new ApkInstallManager(this);
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate:" + this + " " + Thread.currentThread());
+    }
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // FIXME: 2020/8/1 校验权限
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            int checkSelfPermission = checkSelfPermission(PER);
+//            Log.d(TAG, "onBind checkSelfPermission:" + checkSelfPermission);
+//        }
+//
+//        int checkCallingPermission = checkCallingPermission(PER);
+//        Log.d(TAG, "onBind checkCallingPermission:" + checkCallingPermission);
+//        int checkCallingOrSelfPermission = checkCallingOrSelfPermission(PER);
+//        Log.d(TAG, "onBind check:" + checkCallingOrSelfPermission + " " + Thread.currentThread());
+//        if (checkCallingOrSelfPermission == PackageManager.PERMISSION_DENIED) {
+//            //权限不满足，返回NULL，绑定服务失败
+//            return null;
+//        }
+        ZLog.d(TAG, "onBind:" + mBinder + " identityHashCode:" + System.identityHashCode(mBinder));
+        return mBinder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand:" + mBinder + " " + Thread.currentThread());
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+        Log.d(TAG, "onRebind:" + Thread.currentThread());
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, "onUnbind:" + Thread.currentThread());
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy:" + Thread.currentThread());
+    }
+}
